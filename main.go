3739337e69d494277aacc741cc12943aa66ab5bb
maxim.makukha <maxim.makukha@ringteam.com>
2025-01-13 20:04:17 +0200

    Fix ring name

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func check(e error) {
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println("Fake output v0.3")
+		fmt.Println("Fake output v0.4")
 		os.Exit(0)
 	}
 
@@ -25,7 +25,7 @@ func main() {
 		"qr-code-gen.apk":                                  qr_code_gen,
 		"appium-uiautomator2-server-v":                     appium_uiautomator2_server_v,
 		"appium-uiautomator2-server-debug-androidTest.apk": appium_uiautomator2_server_debug_androidTest,
-		"ring-develop":                                     ring_develop,
+		"ring-": ring_develop,
 	}
 
 	for k, v := range paramMap {
